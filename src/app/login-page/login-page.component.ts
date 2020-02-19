61af327a68a5cd28a2d43e95ab53583c70b0cfbb
Sanin Redžepović <sanin.redzepovic@gmail.com>
2020-02-19 17:15:37 +0100

    Small change on validation

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -21,38 +21,47 @@ export class LoginPageComponent implements OnInit {
   ngOnInit() {
     this.authService.removeUserFromStorage();
     this.loginForm = new FormGroup({
-      'email': new FormControl('', { validators: [Validators.required], updateOn: 'blur'}),
-      'password': new FormControl('', { validators: [Validators.required], updateOn: 'blur'}),
+      'email': new FormControl('', { validators: [Validators.required] }),
+      'password': new FormControl('', { validators: [Validators.required] }),
     });
     this.loginFailed = false;
   }
   // login method
   login() {
     this.loginFailed = false;
-    const userForLogin = new User;
-    userForLogin.email = this.email.value;
-    userForLogin.password = btoa(this.password.value);
-    this.userService.CheckUser(userForLogin).subscribe((response: User[]) => {
-      const returnedUser = response[0];
-      if (returnedUser === undefined) {
-        this.loginFailed = true;
-      } else {
-        if ((returnedUser.email === userForLogin.email) && (userForLogin.password === returnedUser.password)) {
-          this.authService.insertUserToStorage(returnedUser);
-          this.router.navigate(['/pages/homepage']);
-        } else {
-          this.loginFailed = true;
-        }
-      }
-    }, () => {
-      this.toastrService.error('Something bad happened!','Error');
+    if (!this.loginForm.valid) {
       this.loginFailed = true;
-    });
+    }
+    else {
+      const userForLogin = new User;
+      userForLogin.email = this.email.value;
+      userForLogin.password = btoa(this.password.value);
+      this.userService.CheckUser(userForLogin).subscribe((response: User[]) => {
+        const returnedUser = response[0];
+        if (returnedUser === undefined) {
+          this.loginFailed = true;
+        } else {
+          if ((returnedUser.email === userForLogin.email) && (userForLogin.password === returnedUser.password)) {
+            this.authService.insertUserToStorage(returnedUser);
+            this.router.navigate(['/pages/homepage']);
+          } else {
+            this.loginFailed = true;
+          }
+        }
+      }, () => {
+        this.toastrService.error('Something bad happened!','Error');
+        this.loginFailed = true;
+      });
+    }
   }
   // reset validation after exiting validation window
   closeValidationInfo(inputValidation: AbstractControl) {
     inputValidation.markAsUntouched();
     inputValidation.markAsPristine();
+  }
+  // reset div
+  reset() {
+    this.loginFailed = false;
   }
   // getters
   get email() { return this.loginForm.get('email') }
